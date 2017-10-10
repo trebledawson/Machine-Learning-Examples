be828ef4e59a2b36ccb25414e6569870c94a15af
Glenn Dawson <trebledawson@gmail.com>
2017-10-10 13:36:17 -0400

    Compartmentalized tests inside functions. Added Fisher iris data test.

--- simple_perceptron.py
+++ simple_perceptron.py
@@ -13,64 +13,15 @@ from numpy.random import multivariate_normal as mvnrnd
 from math import sqrt as sqrt
 import matplotlib.pyplot as plt
 import time
+from sklearn import datasets
+from sklearn.model_selection import train_test_split as tts
 
 ##########
 #  Main  #
 ##########
 
 def main():
-    start = time.time()
-    print('Program start.')
-
-    # Generate training data
-    print('Generating training data...')
-    cases = 1000
-    mu1 = [1.6, 1]
-    mu2 = [3.3, 2.7]
-    mu3 = [1, 3]
-    sigma1 = [[0.1, 0], [0, 0.1]]
-    sigma2 = [[0.1, 0], [0, 0.1]]
-    sigma3 = [[0.1, 0], [0, 0.1]]
-    Q1 = mvnrnd(mu1, sigma1, cases)
-    Q2 = mvnrnd(mu2, sigma2, cases)
-    Q3 = mvnrnd(mu3, sigma3, cases)
-    Q = np.vstack((Q1, Q2, Q3))
-    Y = np.vstack(
-        (np.zeros((cases, 1)), np.ones((cases, 1)), np.full((cases, 1), 2)))
-
-    # Train perceptron classifier
-    print('Training perceptron classifier...')
-    sp = SimplePerceptron(Q, Y)
-
-    # Classification test
-    print('Generating test data...')
-    mu_test = [2.1, 2]
-    sigma_test = [[0.7, 0], [0, 0.7]]
-    Q_test = mvnrnd(mu_test, sigma_test, 10000)
-
-    print('Evaluating test data using perceptron model...')
-    P = sp.predict(Q_test)
-
-    # Plot training data and classifications of test data
-    print('Plotting data...')
-    plt.subplot(121) # Training data
-    plt.plot(Q1[:, 0], Q1[:, 1], 'b.')
-    plt.plot(Q2[:, 0], Q2[:, 1], 'r.')
-    plt.plot(Q3[:, 0], Q3[:, 1], 'g.')
-    plt.title('Plot of randomly generated data')
-    plt.xlim((-0.5, 4.5))
-    plt.ylim((-0.5, 4.5))
-
-    plt.subplot(122) # Test data
-    plt.plot(Q_test[P == 0, 0], Q_test[P == 0, 1], 'b.')
-    plt.plot(Q_test[P == 1, 0], Q_test[P == 1, 1], 'r.')
-    plt.plot(Q_test[P == 2, 0], Q_test[P == 2, 1], 'g.')
-    plt.title('Simple perceptron classification of data')
-    plt.xlim((-0.5,4.5))
-    plt.ylim((-0.5,4.5))
-
-    print('Time elapsed:', time.time()-start, 'seconds.')
-    plt.show()
+    iris_test()
 
 ##################################
 #  Simple Perceptron Classifier  #
@@ -98,7 +49,7 @@ class SimplePerceptron:
 
         print('The perceptron was successfully trained.')
 
-    def gradient_descent(self, class_data, nonclass_data, eta = 0.03):
+    def gradient_descent(self, class_data, nonclass_data, eta = 0.01):
         # Create single array of data and set x_0 to 1
         data = np.vstack((class_data, nonclass_data))
         data = np.hstack((np.ones((data.shape[0], 1)), data))
@@ -137,6 +88,94 @@ class SimplePerceptron:
             classifications.append(selection)
 
         return np.array(classifications)
+
+##############################
+#  User-generated data test  #
+##############################
+
+def user_generated_test():
+    start = time.time()
+    print('Program start.')
+
+    # Generate training data
+    print('Generating training data...')
+    cases = 1000
+    mu1 = [1.6, 1]
+    mu2 = [3.3, 2.7]
+    mu3 = [1, 3]
+    sigma1 = [[0.1, 0], [0, 0.1]]
+    sigma2 = [[0.1, 0], [0, 0.1]]
+    sigma3 = [[0.1, 0], [0, 0.1]]
+    Q1 = mvnrnd(mu1, sigma1, cases)
+    Q2 = mvnrnd(mu2, sigma2, cases)
+    Q3 = mvnrnd(mu3, sigma3, cases)
+    Q = np.vstack((Q1, Q2, Q3))
+    Y = np.vstack(
+        (np.zeros((cases, 1)), np.ones((cases, 1)), np.full((cases, 1), 2)))
+
+    # Train perceptron classifier
+    print('Training perceptron classifier...')
+    sp = SimplePerceptron(Q, Y)
+
+    # Classification test
+    print('Generating test data...')
+    mu_test = [2.1, 2]
+    sigma_test = [[0.7, 0], [0, 0.7]]
+    Q_test = mvnrnd(mu_test, sigma_test, 10000)
+
+    print('Evaluating test data using perceptron model...')
+    P = sp.predict(Q_test)
+
+    # Plot training data and classifications of test data
+    print('Plotting data...')
+    plt.subplot(121)  # Training data
+    plt.plot(Q1[:, 0], Q1[:, 1], 'b.')
+    plt.plot(Q2[:, 0], Q2[:, 1], 'r.')
+    plt.plot(Q3[:, 0], Q3[:, 1], 'g.')
+    plt.title('Plot of randomly generated data')
+    plt.xlim((-0.5, 4.5))
+    plt.ylim((-0.5, 4.5))
+
+    plt.subplot(122)  # Test data
+    plt.plot(Q_test[P == 0, 0], Q_test[P == 0, 1], 'b.')
+    plt.plot(Q_test[P == 1, 0], Q_test[P == 1, 1], 'r.')
+    plt.plot(Q_test[P == 2, 0], Q_test[P == 2, 1], 'g.')
+    plt.title('Simple perceptron classification of data')
+    plt.xlim((-0.5, 4.5))
+    plt.ylim((-0.5, 4.5))
+
+    print('Time elapsed:', time.time() - start, 'seconds.')
+    plt.show()
+
+######################
+#  Fisher iris test  #
+######################
+
+def iris_test():
+    start = time.time()
+    print('Program start.')
+
+    # Load Fisher iris data
+    print('Loading Fisher iris data...')
+    iris = datasets.load_iris()
+    data_train, data_test, target_train, target_test = tts(
+                         iris.data, iris.target, test_size=0.3, random_state=0)
+
+    # Train perceptron classifier
+    print('Training perceptron classifier...')
+    sp = SimplePerceptron(data_train, target_train)
+
+    # Classification of test data
+    predictions = sp.predict(data_test)
+
+    # Evaluation of results
+    evaluation = np.logical_and(target_test, predictions)
+    correct = np.count_nonzero(evaluation)
+    percentage = 100 * correct / len(data_test)
+
+    # Print results
+    print('The classifier accuracy is ', percentage, '%.')
+    print('The elapsed time is', time.time() - start, 'seconds.')
 
 if __name__ == '__main__':
     main()
