6a4206237e34e1c824fbaf5cbd4d67b0abc4ac3c
Glenn Dawson <trebledawson@gmail.com>
2017-10-17 11:40:08 -0400

    Support vector machine corrected. Only supports linearly-separable case with no marginal overlap.

--- Python/support_vector_machine.py
+++ Python/support_vector_machine.py
@@ -9,15 +9,10 @@
 #############
 
 import numpy as np
-from numpy.random import uniform as uniform
 from numpy.random import multivariate_normal as mvnrnd
-from math import sqrt as sqrt
 import matplotlib.pyplot as plt
 import time
-from sklearn import datasets
-from sklearn.model_selection import train_test_split as tts
 from quadprog import solve_qp
-
 
 ##########
 #  Main  #
@@ -48,32 +43,20 @@ class SupportVectorMachine:
         # Add x_0 = 1 to data
         data = np.insert(data, 0, 1, axis=1)
 
-        H = np.eye(self.num_features + 1 + self.num_instances)
-
-        C_w = np.zeros((1, self.num_features + 1))
-        C_xi = np.full((1, self.num_instances), 0.2)
-        f = np.hstack((C_w, C_xi))
-
-        trailing_zeros = np.full((self.num_instances, self.num_instances), -1)
-        A = np.hstack((-labels * data, trailing_zeros)).T
-
+        # quadprog parameters
+        H = np.eye(self.num_features + 1)
+        H[0][0] = 0.00001
+        f = np.ravel(np.zeros((1, self.num_features + 1)))
+        A = labels * data
         b = np.ravel(np.full((1, self.num_instances), -1))
 
-        f = np.ravel(f)
         self.params = self.quadprog_solve_qp(H, f, A, b)
 
         print('The support vector has been obtained.')
         print('Training time:', time.time() - training_start, 'seconds.')
 
-    def predict(self, test_data):
-        print('Classifying data...')
-        data = np.insert(np.array(test_data), 0, 1, axis=1)
-        w = self.params[:self.num_features + 1]
-        classification = np.inner(w, data)
-
-        return np.less_equal(classification, np.zeros(data.shape[0]))
-
     def quadprog_solve_qp(self, P, q, G=None, h=None, A=None, b=None):
+        # Author: Stephane Caron
         qp_G = .5 * (P + P.T) # make sure P is symmetric
         qp_a = -q
         if A is not None:
@@ -81,11 +64,20 @@ class SupportVectorMachine:
             qp_b = -np.hstack([b, h])
             meq = A.shape[0]
         else:  # no equality constraint
-            qp_C = -G
+            qp_C = -G.T
             qp_b = (-h).astype(np.float)
             meq = 0
 
         return solve_qp(qp_G, qp_a, qp_C, qp_b, meq)[0]
+
+    def predict(self, test_data):
+        print('Classifying data...')
+        data = np.insert(np.array(test_data), 0, 1, axis=1)
+        w = self.params[:self.num_features + 1]
+        classification = np.inner(w, data)
+
+        return np.greater(classification, 0)
+
 
 ##################
 #  Test scripts  #
@@ -97,10 +89,10 @@ def test_2d():
 
     # Generate training data
     print('Generating training data...')
-    mu1 = [1, 1]
-    mu2 = [3, 3]
-    sigma1 = [[0.1, 0], [0, 0.1]]
-    sigma2 = [[0.1, 0], [0, 0.1]]
+    mu1 = [0.7, 1]
+    mu2 = [3.7, 3.3]
+    sigma1 = [[0.2, 0], [0, 0.3]]
+    sigma2 = [[0.2, 0], [0, 0.3]]
     Q1 = mvnrnd(mu1, sigma1, 1000)
     Q2 = mvnrnd(mu2, sigma2, 1000)
 
@@ -111,7 +103,7 @@ def test_2d():
     # Generate test data
     print('Generating test data...')
     mu = [2, 2]
-    sigma = [[0.7, 0],[0, 0.7]]
+    sigma = [[0.7, 0], [0, 0.7]]
     Q = mvnrnd(mu, sigma, 10000)
 
     # Classify test data
@@ -128,11 +120,15 @@ def test_2d():
     X = np.linspace(0, 4, 500)
     w = svm.params[:svm.num_features + 1]
     plt.plot(X,(-(w[1]*X+w[0]))/(w[2]))
+    plt.xlim(xmin=-1, xmax=5)
+    plt.ylim(ymin=-1, ymax=5)
 
     plt.subplot(122)
     plt.title('Classified test data')
     plt.plot(Q[P == 0, 0], Q[P == 0, 1], 'b.')
     plt.plot(Q[P == 1, 0], Q[P == 1, 1], 'r.')
+    plt.xlim(xmin=-1, xmax=5)
+    plt.ylim(ymin=-1, ymax=5)
 
     print('Total elapsed time is', time.time() - start, 'seconds.')
     plt.show()
