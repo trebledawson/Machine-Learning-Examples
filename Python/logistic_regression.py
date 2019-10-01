d8343e12b66defc4a1d54e4e1a952d5071aa2e27
Glenn Dawson <trebledawson@gmail.com>
2019-10-01 15:33:48 -0400

    Update logistic_regression.py

--- Python/logistic_regression.py
+++ Python/logistic_regression.py
@@ -1,97 +1,108 @@
-###############################################################################
-# Logistic Regression using Gradient Descent                                  #
-# Glenn Dawson                                                                #
-# 2017-10-06                                                                  #
-###############################################################################
+# ############################################################################ #
+# Logistic Regression using Gradient Descent                                   #
+# Glenn Dawson                                                                 #
+# 2017-10-06                                                                   #
+# ############################################################################ #
 
-#####################
-#  List of Imports  #
-#####################
 import numpy as np
 from numpy.random import multivariate_normal as mvnrnd
+from numpy.random import rand
 import matplotlib.pyplot as plt
 import time
 
-##########
-#  Main  #
-##########
 
 def main():
-    start = time.time()
-    # Generate test data
-    mu1 = [1,1]
-    mu2 = [3,3]
-    sigma1 = [[0.1,0],[0,0.1]]
-    sigma2 = [[0.1,0],[0,0.1]]
-    Q1 = mvnrnd(mu1,sigma1,1000)
-    Q2 = mvnrnd(mu2, sigma2, 1000)
+    # Generate data
+    mu1 = [1, 1]
+    mu2 = [3, 3]
+    sigma1 = [[0.2, 0], [0, 0.1]]
+    sigma2 = [[0.1, 0], [0, 0.2]]
+    q1 = mvnrnd(mu1, sigma1, 1000)
+    q2 = mvnrnd(mu2, sigma2, 1000)
 
     # Perform gradient descent
-    (theta,costs) = gradient_descent(Q1,Q2)
-
-    print(time.time()-start)
+    start = time.time()
+    theta, costs = gradient_descent(q1, q2, alpha=1e-4, epsilon=1e-3)
+    print('Algorithm runtime: {:.4f} seconds'.format(time.time()-start))
+    
     # Plot results
     plt.subplot(121)
-    plt.plot(Q1[:, 0], Q1[:, 1], '.')
-    plt.plot(Q2[:, 0], Q2[:, 1], '.')
+    plt.plot(q1[:, 0], q1[:, 1], '.')
+    plt.plot(q2[:, 0], q2[:, 1], '.')
+    plt.grid()
     plt.title('Plot of randomly generated data')
 
-    X = np.linspace(0,5,500)
-    plt.plot(X,(-(theta[1]*X+theta[0]))/(theta[2]))
+    log_reg_clf = np.linspace(-.25, 4.25, 500)
+    plt.plot(log_reg_clf, (-(theta[1] * log_reg_clf + theta[0])) / (theta[2]))
 
     plt.subplot(122)
     plt.plot(costs)
+    plt.grid()
     plt.title('Cost function values over gradient descent iterations')
     plt.show()
 
-###############
-#  Functions  #
-###############
 
-#################################################################
-#  Gradient descent algorithm for logistic regression           #
-#  -----------                                                  #
-#  Arguments:                                                   #
-#  X1 and X2 are arrays of 2D data                              #
-#  theta is the initial fit value                               #
-#  alpha is the learning rate                                   #
-#################################################################
+def gradient_descent(data_a, data_b, alpha=1e-3, epsilon=1e-2, theta=rand(3)):
+    """
+    Gradient descent for logistic regression.
 
-def gradient_descent(X1, X2, theta=np.array([0.0,0.0,0.0]), alpha=0.0001):
-    # Add X_0 = 1 to X1, X2 and concatenate
-    X1 = np.hstack((np.ones(X1.shape[0]).reshape(-1, 1), np.array(X1)))
-    X2 = np.hstack((np.ones(X2.shape[0]).reshape(-1, 1), np.array(X2)))
-    X = np.vstack((X1,X2))
+    Parameters
+    ----------
+    data_a : array-like, shape = (n_samples, n_features)
+        Data corresponding to class A.
+
+    data_b : array-like, shape = (n_samples, n_features)
+        Data corresponding to class B.
+
+    alpha : float
+        Learning rate.
+
+    epsilon : float
+        Stopping criterion.
+
+    theta : array-like, shape = (3)
+        Initial guess for weights.
+
+    Returns
+    -------
+    theta : array, shape = (3)
+        Final weights for logistic regression classification.
+
+    costs : list, shape = (n_steps)
+        History of costs over algorithm runtime.
+    """
+    # Prepend 0 to data vectors
+    data_a = np.insert(data_a, 0, 1, axis=1)
+    data_b = np.insert(data_b, 0, 1, axis=1)
+    data = np.vstack((data_a, data_b))
 
     # Create labels
-    Y = np.concatenate((np.zeros(X1.shape[0]),np.ones(X2.shape[0])))
+    targets = np.concatenate((np.zeros(data_a.shape[0]),
+                              np.ones(data_b.shape[0])))
 
     # Gradient descent algorithm using logistic function
     dCost = 1
     costs = []
-    count = 0
-    while dCost > 0.001:
+    first = True
+    while dCost > epsilon:
         # Calculate error
-        H = 1/(1+np.exp(-(np.inner(theta,X))))
+        logits = 1 / (1 + np.exp(-(data @ theta)))
 
         # Update theta
-        dJ = np.zeros((X.shape[0],X.shape[1]))
-        for j in range(X.shape[0]):
-            dJ[j,:] = ((H[j]-Y[j])*X[j,:])
-        theta = theta - alpha*np.sum(dJ,axis=0)
+        grad = (data.T * (logits - targets)).T
+        theta = theta - alpha * np.sum(grad, axis=0)
 
         # Calculate cost
-        J = np.sum(-Y * np.log(H) - (1 - Y) * np.log(1 - H))
+        cost = np.sum(-targets * np.log(logits)
+                      - (1 - targets) * np.log(1 - logits))
 
-        if count > 0:
-            dCost = abs(costs[-1] - J)
+        if first:
+            dCost = abs(cost)
+            first = False
         else:
-            dCost = abs(J)
-
-        costs.append(J)
-
-        # Increase counter
-        count += 1
+            dCost = abs(costs[-1] - cost)
+            
+        costs.append(cost)
 
     return theta, costs
 
