e7f937aca4c02692aaf71973d91adff156e860af
Glenn Dawson <trebledawson@gmail.com>
2019-10-01 13:35:52 -0400

    Update linear_regression.py

--- Python/linear_regression.py
+++ Python/linear_regression.py
@@ -1,84 +1,104 @@
-###############################################################################
-# Linear Regression using Gradient Descent                                    #
-# Glenn Dawson                                                                #
-# 2017-10-06                                                                  #
-###############################################################################
+# ############################################################################ #
+# Linear Regression using Gradient Descent                                     #
+# Glenn Dawson                                                                 #
+# 2017-10-06 (updated 2019)                                                    #
+# ############################################################################ #
 
-#####################
-#  List of Imports  #
-#####################
 import numpy as np
-from numpy.random import rand as rand
+from numpy.random import rand
+from numpy.random import normal
 import matplotlib.pyplot as plt
 import time
 
-##########
-#  Main  #
-##########
+
 def main():
-    start = time.time()
+    # Data definition
     b = 7
     m = 2
     n = 100
-    noise = rand(n,1)
-    X = rand(n,1)*10
-    Y = m*X + b + noise
-
-    (theta,costs) = gradient_descent(X,Y)
-
-    print(time.time() - start)
-
+    X = rand(n, 1) * 10
+    Y = ((m * X + b).T + normal(size=len(X))).T
+    
+    # Gradient descent algorithm
+    start = time.time()
+    theta, costs = gradient_descent(X, Y)
+    print('Algorithm runtime: {:.4f} seconds'.format(time.time() - start))
+    
+    # Plotting
     plt.figure(1)
     plt.subplot(121)
-    plt.plot(X,Y,'.')
+    plt.plot(X, Y, '.')
     plt.title('Plot of randomly generated data')
-    plt.plot(X,theta[1]*X + theta[0])
+    plt.plot(X, theta[1]*X + theta[0])
+    plt.grid()
 
     plt.subplot(122)
     plt.plot(costs)
+    plt.grid()
     plt.title('Cost function values over gradient descent iterations')
     plt.show()
 
-###############
-#  Functions  #
-###############
 
-###########################################################
-#  Gradient descent algorithm for linear regression       #
-#  -----------                                            #
-#  Arguments:                                             #
-#  X is a set of independent variables                    #
-#  Y is the set of dependent variables according to Y(X)  #
-#  theta is the initial fit value                         #
-#  alpha is the learning rate                             #
-###########################################################
+def gradient_descent(data, targets, theta=rand(2), alpha=0.001, epsilon=1e-9):
+    """An implementation of the gradient descent algorithm.
 
-def gradient_descent(X, Y, theta = np.array([0.0,0.0]),alpha = 0.001):
-    X = np.hstack((np.ones(len(X)).reshape(-1,1),X))
+    Returns two-dimensional model weights for y = mx + b in the form theta = (
+    m, b), as well as a history of costs for plotting.
+
+    Parameters
+    ----------
+    data : array-like, shape = (n_samples)
+           Test samples.
+
+    labels : array like, shape = (n_samples)
+             Noisy targets for data.
+
+    theta : array-like, shape = (2)
+            An initial guess to the model weights.
+
+    alpha : float
+            Learning rate.
+
+    epsilon : float
+              Stopping criterion.
+
+    Returns
+    -------
+    theta : array-like, shape = (2)
+            Correct model weights.
+
+    costs : array-like, shape = (n_steps)
+            History of costs over the runtime of the algorithm.
+    """
+    # Prepend data instances with 1 in the 0th index
+    data = np.insert(data, 0, 1, axis=1)
+    
+    # Gradient descent
+    scaler = 2 * len(data)
     delta_cost = 1
     costs = []
-    count = 0
-    while delta_cost > 0.000000001:
-        # Calculating error
-        H = np.inner(theta,X) - Y.T
-
+    first = True
+    while delta_cost > epsilon:
+        # Calculating residual
+        residual = np.ravel((data @ theta) - targets.T)
+        
         # Updating theta
-        for i in range(len(theta)):
-            J = np.multiply(H, X[:,i])
-            theta[i] = theta[i] - alpha * np.sum(J)/(2*len(X))
+        theta = theta - alpha * (residual @ data) / scaler
 
         # Computing LMS cost function
-        cost = np.sum(np.power(H,2)) / (2*len(X))
+        cost = np.sum(np.power(residual, 2)) / scaler
 
         # Checking delta_cost
-        if count > 0:
-            delta_cost = abs(costs[-1] - cost)
-        else:
+        if first:
             delta_cost = abs(cost)
-
+            first = False
+        else:
+            delta_cost = abs(costs[-1] - cost)
+        
+        # Update costs history
         costs.append(cost)
-        count += 1
-    return theta,costs
+        
+    return theta, costs
 
 
 if __name__ == '__main__':
